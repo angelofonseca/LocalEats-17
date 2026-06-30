2ac96470bbf1f9138b6308453e4fa5a199250cc8
Angelo Fonseca <angeloniemeyer97@gmail.com>
2026-06-30 23:53:16 +0000

    ajuste test regressao

--- tests/test_search_regressao.py
+++ tests/test_search_regressao.py
@@ -1,5 +1,7 @@
 from search import RestaurantSearch
+RESTAURANTES = ["Restaurante Sabor 0", "Restaurante Sabor 1", "Restaurante Sabor 2"]
 
 def test_regressao_busca_com_espacos_extras_encontra_restaurante():
+    busca = RestaurantSearch(RESTAURANTES)
     assert busca.search(" Restaurante Sabor 0 ") == []
     assert busca.search("Restaurante Sabor 1 ") == []

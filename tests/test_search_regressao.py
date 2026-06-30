f22ad180614222c25289e1ca4a6cdc01d47850e1
Angelo Fonseca <angeloniemeyer97@gmail.com>
2026-06-30 23:50:43 +0000

    remove busca inexistente

--- tests/test_search_regressao.py
+++ tests/test_search_regressao.py
@@ -1,6 +1,5 @@
 from search import RestaurantSearch
 
 def test_regressao_busca_com_espacos_extras_encontra_restaurante():
-    busca = RestaurantSearch(RESTAURANTES)
     assert busca.search(" Restaurante Sabor 0 ") == []
     assert busca.search("Restaurante Sabor 1 ") == []
